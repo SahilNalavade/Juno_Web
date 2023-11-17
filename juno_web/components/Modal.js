381725fb9b765f8ecdfaa899caa061d87f10d5e9
SahilNalavade <sahilnalavade25@gmail.com>
2023-11-18 00:56:55 +0530

    modal completed

---
+++ juno_web/components/Modal.js
@@ -0,0 +1,141 @@
+// components/Modal.js
+import { useState } from 'react';
+import styles from '@/styles/Modal.module.css';
+
+const Modal = ({ handleClose }) => {
+  const [email, setEmail] = useState('');
+  const [fileUAR, setFileUAR] = useState(''); // You can use true/false for Yes/No options
+  const [chargeClosureFee, setChargeClosureFee] = useState(''); // Initial value can be an empty string or 'yes'/'no'
+
+  const [reason, setReason] = useState('');
+  const [note, setNote] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic to handle form submission
+  };
+
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <h2>Close account</h2>
+          <button onClick={handleClose} className={styles.closeButton}>
+            &#10005;
+          </button>
+        </div>
+        <form onSubmit={handleFormSubmit} className={styles.form}>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={`${styles.formGroup} ${styles.fileUAR}`}>
+            <label htmlFor="fileUAR">Want to file UAR:</label>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="fileUAR"
+                  value="yes"
+                  checked={fileUAR === 'yes'}
+                  onChange={() => setFileUAR('yes')}
+                />
+                Yes
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="fileUAR"
+                  value="no"
+                  checked={fileUAR === 'no'}
+                  onChange={() => setFileUAR('no')}
+                />
+                No
+              </label>
+            </div>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="reason">Reason</label>
+            <select
+  id="reason"
+  value={reason}
+  onChange={(e) => setReason(e.target.value)}
+  className="form-input" // You can add a class for styling if needed
+  style={{
+    width: '100%',
+    padding: '8px',
+    boxSizing: 'border-box',
+    marginTop: '5px',
+    background: '#fff',
+    color: '#000',
+    border: '1px solid #ccc',
+    borderRadius: '4px', // Added border radius for consistency
+  }}
+>
+  <option value="option1"></option>  
+  <option value="option2">Unauthorized charges</option>
+  <option value="option3">Incorrect amounts</option>
+  <option value="option4">Missing transactions</option>
+  <option value="option5">To report a customer service complaint</option>
+  <option value="option6">To report suspected fraud or identity theft
+</option>
+</select>
+
+
+          </div>
+          <div className={styles.formGroup}>
+          <label htmlFor="note">Note</label>
+<textarea
+  id="note"
+  value={note}
+  onChange={(e) => setNote(e.target.value)}
+  style={{
+    resize: 'none', // Disable resizing
+    height:'65px'
+    
+  }}
+/>
+          </div>
+          <div className={`${styles.formGroup} ${styles.fileUAR}`}>
+           
+          <label>
+  <input
+    type="radio"
+    name="chargeClosureFee"
+    value="yes"
+    checked={chargeClosureFee === 'yes'}
+    onChange={() => setChargeClosureFee('yes')}
+  />
+  <span style={{ color: '#ADADAD' }}>Charge closure fee</span>
+</label>
+
+<button
+  style={{
+    width: '211px',
+    height: '48px',
+    padding: '16px',
+    borderRadius: '8px',
+    marginLeft: '8px',
+    backgroundColor: '#E4E4E4', // Add your desired background color here
+    border:'none',
+    color:'#ADADAD',
+    marginLeft:'70px'
+  }}
+>
+  Close Account
+</button>
+
+</div>
+          
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ juno_web/components/Navbar.js
@@ -0,0 +1,35 @@
+// components/Navbar.js
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from '@/styles/Navbar.module.css';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav  className={`${styles.navbar} ${isMenuOpen ? styles.open : ''}`}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      <ul className={`${styles.navList} ${isMenuOpen ? styles.show : ''}`}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
