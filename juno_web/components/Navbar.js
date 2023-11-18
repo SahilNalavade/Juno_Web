1683551f6da357a10d4e17ef8924771f5333e5dc
SahilNalavade <sahilnalavade25@gmail.com>
2023-11-18 06:29:46 +0530

    Nav done

--- juno_web/components/Navbar.js
+++ juno_web/components/Navbar.js
@@ -1,32 +1,60 @@
 // components/Navbar.js
 import { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '@/styles/Navbar.module.css';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const router = useRouter();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav  className={`${styles.navbar} ${isMenuOpen ? styles.open : ''}`}>
+    <nav className={`${styles.navbar} ${isMenuOpen ? styles.open : ''}`}>
       <div className={styles.hamburger} onClick={toggleMenu}>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
       </div>
+
       <ul className={`${styles.navList} ${isMenuOpen ? styles.show : ''}`}>
         <li>
-          <Link href="/">Home</Link>
+          <img src='/logo.png' alt="Logo" style={{ width: '194px', marginTop: '48px', marginBottom: '48px' }} />
         </li>
-        <li>
-          <Link href="/about">About</Link>
+        <li className={router.pathname === '/overview' ? styles.selected : ''}>
+          <Link href="/overview">Overview</Link>
         </li>
-        <li>
-          <Link href="/contact">Contact</Link>
+        <li className={router.pathname === '/onboarding' ? styles.selected : ''}>
+          <Link href="/onboarding">Onboarding</Link>
         </li>
+        <li className={router.pathname === '/' ? styles.selected : ''}>
+          <Link href="/">Monitoring</Link>
+        </li>
+        <li className={router.pathname === '/flagging' ? styles.selected : ''}>
+          <Link href="/flagging">Flagging</Link>
+        </li>
+        <li className={router.pathname === '/src' ? styles.selected : ''}>
+          <Link href="/src">Source of Income</Link>
+        </li>
+        <li className={router.pathname === '/uar' ? styles.selected : ''}>
+          <Link href="/uar">UAR</Link>
+        </li>
+
+        <div className={styles.userContainer}>
+        <li className={styles.userSection}>
+          <div className={styles.userImageWrapper}>
+            <img src='/elon.jpg' alt="User" className={styles.userImage} />
+          </div>
+          <div>
+            <div className={styles.userName}>Elon Musk</div>
+            <div className={styles.additionalName}>elon@twitter.com</div>
+          </div>
+        </li>
+      </div>
+    
       </ul>
     </nav>
   );
